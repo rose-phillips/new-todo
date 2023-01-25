c3f555a9bb72d18c2db33020adfacc9cf9b0f140
ROSE PHILLIPS (U31102) <rophillips@ybs.co.uk>
2023-01-25 14:07:16 +0000

    push to test local storage on macbook

---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo({todo}) {
+  return (
+    <div>
+     <label>
+         <input type="checkbox" checked={todo.complete} />
+     {todo.name}
+     </label>
+    </div>
+  )
+}
+
+export default Todo
